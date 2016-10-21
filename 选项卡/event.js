b2c5965c1218078d51150547163077ce0b817942
fountainHe <abc200868@126.com>
2016-10-21 17:04:46 +0800

    add notes

--- 选项卡/event.js
+++ 选项卡/event.js
@@ -7,6 +7,7 @@ window.onload = function() {
   for (var i = 0,len=olis.length; i < len; i++) {
     olis[i].index = i;
     olis[i].onclick = function() {
+      // 首先要清除掉原来的某个已被选中的样式
       for (var n = 0; n < len; n++) {
         olis[n].className = "";
         oDivs[n].className = "hide";
